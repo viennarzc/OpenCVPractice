9c517c64a7f88364ce2f1b59c543e09859dfa977
Viennarz Curtiz <viennarzc@gmail.com>
2020-03-09 14:36:40 +0800

    Initial Commit

---
+++ OpenCVPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenCVPractice
+//
+//  Created by SCI-Viennarz on 3/9/20.
+//  Copyright © 2020 SCI-Viennarz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
